ba5e375368da30376e288f75a2726b529d3fc9ac
Sam Chapman <schap843@gmail.com>
2016-06-05 16:03:04 +0900

    refactor code

--- models/Posts.js
+++ models/Posts.js
@@ -3,14 +3,10 @@ var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
   title: { type: String, required: '{PATH} is required!'},
-  link: String,
   body: String,
   isPost: Boolean,
-  created_at: { type: Date, default: Date.now },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   author: String
 });
-
-
 
 mongoose.model('Post', PostSchema);
