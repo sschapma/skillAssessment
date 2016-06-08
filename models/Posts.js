0d226c7934fff8ce1d9bca5d349c5886103b6b1f
Sam Chapman <schap843@gmail.com>
2016-06-08 13:09:19 +0900

    Update Posts.js

--- models/Posts.js
+++ models/Posts.js
@@ -5,6 +5,7 @@ var PostSchema = new mongoose.Schema({
   title: { type: String, required: '{PATH} is required!'},
   body: String,
   isPost: Boolean,
+  createdOn: { type: Date, default: Date.now },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   author: String
 });
