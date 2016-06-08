1345bb31cffe9b69e136ac9fd01d340e6af47342
Sam Chapman <schap843@gmail.com>
2016-06-08 13:09:43 +0900

    Update Comments.js

--- models/Comments.js
+++ models/Comments.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
   body: String,
   author: String,
+  createdOn: { type: Date, default: Date.now },
   post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
 
